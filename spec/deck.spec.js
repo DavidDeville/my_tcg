08c251f48951f9fe2f75fba81598bb8b5940dbf4
David <david.de-ville@epitech.eu>
2020-04-22 05:31:02 -0400

    added a test for the shuffle method and fixed some stuff

--- spec/deck.spec.js
+++ spec/deck.spec.js
@@ -1,13 +1,34 @@
 import Deck from "../src/models/deck.js";
 
+/**
+ * Tests for the Deck Class
+ * 
+ * Tested methods : shuffle(), insertAt, draw(), getCardsCount() 
+ */
 describe("deck", function () {
+
+  /**
+   * Tests of shuffle method
+   */
+  describe("shuffle", () => {
+    it("cards have been shuffled", () => {
+      const config = { cards: [7,10,9,90,12] };
+      const deck = new Deck(config);
+      deck.shuffle();
+      console.log(deck.cards);
+      expect(deck.shuffle()).toBe(true);
+    });
+  });
+
+  /**
+   * Tests of getCardsCount method
+   */
   describe("cards count", function () {
     it("should be an integer", function () {
-      const config = { cards: ["pouet"] };
+      const config = { cards: [777] };
       const deck = new Deck(config);
-      let numberOfCards = deck.cards[0];
-      console.log(numberOfCards);
-      expect(numberOfCards).toBe(1);
+      let card = deck.cards[0];
+      expect(card).toMatch(/^\d+$/);
     });
   });
 });
--- src/models/deck.js
+++ src/models/deck.js
@@ -5,6 +5,20 @@ export default class Deck {
     }
 
     shuffle () {
+        let shuffledArray = [];
+        let i = this.cards.length, j, temp;
+
+        if(this.cards.length < 1) {
+            return false;
+        }
+        while(--i > 0) {
+            j = Math.floor(Math.random() * (i+1));
+            temp = this.cards[j];
+            this.cards[j] = this.cards[i];
+            this.cards[i] = temp;
+            shuffledArray = this.cards;
+        }
+        return true;
     }
 
     draw () {
